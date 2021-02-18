729d52666e9ab0bee6dd7e922da0c0c9e6e5e5b8
Zhandos <yernazarovz@gmail.com>
2021-02-18 13:16:22 +0500

    Initial Commit

---
+++ Mapper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mapper
+//
+//  Created by Zhandos Yernazarov on 2/18/21.
+//  Copyright © 2021 Zhandos Yernazarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
